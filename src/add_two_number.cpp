458c09a551b76313068086e7ddedf67a44a6bf20
Zhiliang Zhou <zhouzhiliang@gmail.com>
2020-11-03 23:44:06 +0100

    add leetcode 2

--- src/add_two_number.cpp
+++ src/add_two_number.cpp
@@ -14,47 +14,41 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     int carry = 0;
     int mod = 0;
     // calculate the one before last overlapped element
-    while (l1->next != nullptr && l2->next != nullptr){
+    while (true) {
         mod = (l1->val + l2->val + carry) % 10;
-        carry = (l1->val + l2->val) / 10;
-
+        carry = (l1->val + l2->val + carry) / 10;
         digit_curr->val = mod;
-        ListNode* next_digit = new ListNode();
-        digit_curr->next = next_digit;
-        digit_curr = next_digit;
-        l1 = l1->next;
-        l2 = l2->next;
-    }
-    // calculate the last overlapped element
-    mod = (l1->val + l2->val + carry) % 10;
-    carry = (l1->val + l2->val) / 10;
-    digit_curr->val = mod;
-    digit_curr->next = new ListNode();
-    digit_curr = digit_curr->next;
+
+        if (l1->next != nullptr && l2->next != nullptr){
+            digit_curr->next = new ListNode();
+            digit_curr = digit_curr->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }else{
+            digit_curr->next = nullptr;
+            break;
+        }
+    } 
 
     // calculate non overlaped
-    if (l1->next !=0){
-        while (l1->next !=0 ){
-            mod = (l1->val + carry) % 10;
-            carry = (l1->val) / 10;
-            digit_curr->val = mod;
-            ListNode* next_digit = new ListNode();
-            digit_curr->next = next_digit;
-            digit_curr = next_digit;
+    if (l1->next != nullptr){
+        do {
             l1 = l1->next;
-        }
+            mod = (l1->val + carry) % 10;
+            carry = (l1->val + carry) / 10;
+            digit_curr->next = new ListNode(mod);
+            digit_curr = digit_curr->next;
+        } while (l1->next != nullptr);
     }else if(l2->next !=0){
-        while (l2->next !=0 ){
-            mod = (l2->val + carry) % 10;
-            carry = (l2->val) / 10;
-            digit_curr->val = mod;
-            ListNode* next_digit = new ListNode();
-            digit_curr->next = next_digit;
-            digit_curr = next_digit;
+        do {
             l2 = l2->next;
-        }
+            mod = (l2->val + carry) % 10;
+            carry = (l2->val  + carry) / 10;
+            digit_curr->next = new ListNode(mod);
+            digit_curr = digit_curr->next;
+        } while (l2->next != nullptr);
     }
-    // last digit
+
     if (carry!=0){
         digit_curr->next = new ListNode(carry, nullptr); // assert carray = 1
     }
@@ -62,41 +56,61 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     return digit_head;
 }
 
-int main(int argv, char* argc[])
-{
+void printout(ListNode* n_head){
+    ListNode* n_cur = n_head;
+    while (n_cur != nullptr){
+        std::cout << n_cur->val << ", ";
+        n_cur = n_cur->next;
+    }
+    std::cout << std::endl;
+}
+
+void test1(){
+    ListNode* n1_head = new ListNode(2);
+    ListNode* n1_cur = n1_head;
+    n1_cur->next = new ListNode(4);
+    n1_cur = n1_cur->next;
+    n1_cur->next = new ListNode(3);
+    n1_cur = n1_cur->next;
+
+    ListNode* n2_head = new ListNode(5);
+    ListNode* n2_cur = n2_head;
+    n2_cur->next = new ListNode(6);
+    n2_cur = n2_cur->next;
+    n2_cur->next = new ListNode(4);
+    n2_cur = n2_cur->next;
+
+    printout(n1_head);
+    printout(n2_head);
+
+    ListNode* res_head = addTwoNumbers(n1_head, n2_head);
+    printout(res_head);
+}
+
+void test2(){
     ListNode* n1_head = new ListNode(9);
     ListNode* n1_cur = n1_head;
-    for (int i=0; i<10; i++){
+    for (int i = 0; i < 8; i++){
         n1_cur->next = new ListNode(9);
         n1_cur = n1_cur->next;
     }
 
     ListNode* n2_head = new ListNode(9);
     ListNode* n2_cur = n2_head;
-    for (int i=0; i<4; i++){
+    for (int i = 0; i < 5; i++){
         n2_cur->next = new ListNode(9);
         n2_cur = n2_cur->next;
     }
 
-    n1_cur = n1_head;
-    while (n1_cur != nullptr){
-        std::cout << n1_cur->val << ", ";
-        n1_cur = n1_cur->next;
-    }
-    std::cout << std::endl;
-
-    n2_cur = n2_head;
-    while (n2_cur != nullptr){
-        std::cout << n2_cur->val << ", ";
-        n2_cur = n2_cur->next;
-    }
-    std::cout << std::endl;
+    printout(n1_head);
+    printout(n2_head);
 
     ListNode* res_head = addTwoNumbers(n1_head, n2_head);
-    ListNode* res_cur = res_head;
-    while (res_cur != nullptr){
-        std::cout << res_cur->val << ", ";
-        res_cur = res_cur->next;
-    }
-    std::cout << std::endl;
+    printout(res_head);
+}
+
+int main(int argv, char* argc[])
+{
+    test1();
+    test2();
 }
