f1abbe007c12ccefb86860578599c3c9d257fc92
Zhiliang Zhou <zhouzhiliang@gmail.com>
2020-11-19 10:56:38 +0100

    update: change order

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,29 @@
 // Created by zzhou on 11.11.20.
 //
 
-#include "house_robber.h"
-#include "add_two_number.h"
-#include "find_median_sorted_arrays.h"
-#include "longest_substring_without_repeating_characters.h"
-#include "longest_palindromic_substring.h"
 #include "two_sum.h"
+#include "add_two_number.h"
+#include "longest_substring_without_repeating_characters.h"
+#include "find_median_sorted_arrays.h"
+#include "longest_palindromic_substring.h"
 #include "zigzag_conversion.h"
 #include "reverse_integer.h"
 #include "string_to_integer.h"
 
+#include "house_robber.h"
+
 int main(int argc, char* argv[]){
 
-    leetcode::HouseRobber::test();
-    leetcode::AddTwoNumbers::test();
-    leetcode::FindMedianSortedArrays::test();
-    leetcode::LongestSubStringWithoutRepeatingCharacters::test();
-    leetcode::LongestPalidromicSubString::test();
     leetcode::TwoSum::test();
+    leetcode::AddTwoNumbers::test();
+    leetcode::LongestSubStringWithoutRepeatingCharacters::test();
+    leetcode::FindMedianSortedArrays::test();
+    leetcode::LongestPalidromicSubString::test();
     leetcode::ZigZagConversion::test();
     leetcode::ReverseInteger::test();
     leetcode::StringToInteger::test();
+
+    leetcode::HouseRobber::test();
 
     return 0;
 }
